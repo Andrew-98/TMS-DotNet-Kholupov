a0765bd0c97c419ced04b118bd01c683be2d3c6a
AndrewKh <akholupov@gmail.com>
2020-09-07 20:41:36 +0300

    feat: complete homework 2

---
+++ src/Homework 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you date: ");
+            string userInput = Console.ReadLine();
+            bool result = DateTime.TryParse(userInput, out DateTime date);
+            if (result)
+            {
+                Console.WriteLine(DayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
