db8bb69bbd779a5f5fcd5676dc0bd73a9f98ba27
AndrewKh <akholupov@gmail.com>
2020-12-02 13:08:17 +0300

    feat: add enum

---
+++ src/Homework7/Homework7Core/Enums/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7Core.Enums
+{
+    public enum Gender
+    {
+        Unknow = -1,
+        Female = 0,
+        Male = 1,
+    }
+}
